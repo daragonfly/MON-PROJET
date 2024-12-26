4ef4af3ec6e1838e35c96e53ee5a3bcf8359d6d1
Arthur Girardot <arthur.girardot@edu.devinci.fr>
2024-12-26 20:10:26 +0100

    tout marche i guess: à faire = visuel

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { UsersComponent } from './users/users.component';
+  import { NgModule } from '@angular/core';
+  import { RouterModule, Routes } from '@angular/router';
+  import { UsersComponent } from './users/users.component';
 
-export const appRoutes: Routes = [
-  { path: 'users', component: UsersComponent },
-  { path: '', redirectTo: '/users', pathMatch: 'full' },
-];
+  export const appRoutes: Routes = [
+    { path: 'users', component: UsersComponent },
+    { path: '', redirectTo: '/users', pathMatch: 'full' },
+  ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+  @NgModule({
+    imports: [RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
